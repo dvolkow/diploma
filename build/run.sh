a5233d328c299f61775d21a5e9c99fb890298db4
Daniel Wolkow <volkov12@rambler.ru>
2018-10-09 03:38:08 +0300

    scripts: run.sh for fast result saves

--- build/run.sh
+++ build/run.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 
 ORD=$1
-./diploma ${ORD} apogee-rc-DR14_t1.txt & cat result.txt
+IN_FILE=$2
+L=0
+H=$3
+./diploma -f ${IN_FILE} --ord ${ORD} --filter L ${L} ${H}
+gnuplot ../sources/scripts/main.gnu
+NEW_DIR=$(echo "result_${ORD}_${L}_${H}_${IN_FILE}")
+mkdir ${NEW_DIR}
+
+mv ./sun.txt ${NEW_DIR}
+mv ./averages.txt ${NEW_DIR}
+mv ./rotc.eps ${NEW_DIR}
+mv ./rotc.txt ${NEW_DIR}
+mv ./result.txt ${NEW_DIR}
+mv ./objs.txt ${NEW_DIR}
