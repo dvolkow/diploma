901d9db16ff1f1738b0813180d32531981c50ece
Daniel Wolkow <volkov12@rambler.ru>
2019-05-10 04:59:17 +0300

    runners update: new format for args

--- build/build.sh
+++ build/build.sh
@@ -3,3 +3,6 @@
 export CC=/usr/bin/clang-6.0
 export ASAN_OPTIONS=fast_unwind_on_malloc=0
 cmake .. -DCMAKE_C_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined -g" -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined" -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined" && make
+
+cp ../data/apogee-rc-DR14_hsoy_with_err.txt ./apogee-hsoy.txt
+cp ../data/apogee-rc-DR14_ucac_with_err.txt ./apogee-ucac.txt
--- build/run.sh
+++ build/run.sh
@@ -2,44 +2,46 @@
 
 ORD=$1
 IN_FILE=$2
-L=0
 H=$3
 
 if [[ -n "$4" ]]; then
-        ./diploma -f ${IN_FILE} --ord ${ORD} --filter L ${L} ${H} --mode $4
-        NEW_DIR=$(echo "result_${ORD}_${L}_${H}_$(echo ${IN_FILE}_$4 | sed 's/.txt//')")
+        ./diploma -f ${IN_FILE} --ord ${ORD} --mksize ${H} -b $4
+        NEW_DIR=$(echo "result_${ORD}__${H}_$(echo ${IN_FILE}_$4 | sed 's/.txt//')")
 elif [[ -n "$3" ]]; then
-        ./diploma -f ${IN_FILE} --ord ${ORD} --filter L ${L} ${H}
-        NEW_DIR=$(echo "result_${ORD}_${L}_${H}_$(echo ${IN_FILE} | sed 's/.txt//')")
+        ./diploma -f ${IN_FILE} --ord ${ORD} --mksize ${H} -b 1
+        NEW_DIR=$(echo "result_${ORD}_${H}_$(echo ${IN_FILE} | sed 's/.txt//')")
 else 
         ./diploma -f ${IN_FILE} --ord ${ORD} 
         NEW_DIR=$(echo "result_${ORD}_$(echo ${IN_FILE} | sed 's/.txt//')")
 fi
 gnuplot ../sources/scripts/main.gnu
-gnuplot ../sources/scripts/background.gnu
+#gnuplot ../sources/scripts/background.gnu
 gnuplot ../sources/scripts/xy.gnu
 gnuplot ../sources/scripts/yz.gnu
 gnuplot ../sources/scripts/xz.gnu
-gnuplot ../sources/scripts/lb.gnu
+#gnuplot ../sources/scripts/lb.gnu
 
 mkdir ${NEW_DIR}
 
 mv ./sun.txt ${NEW_DIR}
-mv ./averages.txt ${NEW_DIR}
-mv ./dump_table.txt ${NEW_DIR}
-mv ./rotc.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4.eps
+#mv ./averages.txt ${NEW_DIR}
+#mv ./dump_table.txt ${NEW_DIR}
+mv ./rotc.eps ${NEW_DIR}/${IN_FILE}_${ORD}.eps
 mv ./rotc.txt ${NEW_DIR}
-mv ./background.txt ${NEW_DIR}
+#mv ./background.txt ${NEW_DIR}
 mv ./result.txt ${NEW_DIR}
 mv ./unfresult.txt ${NEW_DIR}
 mv ./objs.txt ${NEW_DIR}
 #mv ./bk_sd.txt ${NEW_DIR}
-mv ./xyz_obj.txt ${NEW_DIR}
-mv ./back.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4_back.eps
-mv ./xy.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4_xy.eps
-mv ./xz.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4_xz.eps
-mv ./yz.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4_yz.eps
-mv ./lb.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${L}_${H}$4_lb.eps
+#mv ./xyz_obj.txt ${NEW_DIR}
+mv ./get_solution_178_0 ${NEW_DIR}
+mv ./ERROR_LIMITED ${NEW_DIR}
+mv ./ERROR_LIMITED_R_THETA ${NEW_DIR}
+#mv ./back.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${H}$4_back.eps
+mv ./xy.eps ${NEW_DIR}/${IN_FILE}_${ORD}_xy.eps
+mv ./xz.eps ${NEW_DIR}/${IN_FILE}_${ORD}_xz.eps
+mv ./yz.eps ${NEW_DIR}/${IN_FILE}_${ORD}_yz.eps
+#mv ./lb.eps ${NEW_DIR}/${IN_FILE}_${ORD}_${H}$4_lb.eps
 
 cat ${NEW_DIR}/result.txt
 #cat ${NEW_DIR}/bk_sd.txt
