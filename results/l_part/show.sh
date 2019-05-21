b79fdc202d95fe8f33728bc6f5bbdd1ac5ba07a7
Daniel Wolkow <volkov12@rambler.ru>
2019-05-21 12:07:10 +0300

    diploma text updated

--- results/l_part/show.sh
+++ results/l_part/show.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 for (( i=$1; i<=$2; i++))
 do
-        cat ${i}/unfresult.txt
+        cat ${i}/r0_bounds.txt
 done 
 
 #        gnuplot -c ../../sources/scripts/main_cmd.gnu "cur.eps" 'objs.txt' 'ERROR_LIMITED_R_THETA'
