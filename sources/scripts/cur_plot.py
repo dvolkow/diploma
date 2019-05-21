5758a33bd3c4dbefdd37a3df87bb0e5d260d1df2
Daniel Wolkow <volkov12@rambler.ru>
2019-05-21 22:33:19 +0300

    after NII conversation

--- sources/scripts/cur_plot.py
+++ sources/scripts/cur_plot.py
@@ -9,8 +9,10 @@ import sys
 
 
 FILE_OBJ=sys.argv[1]
+fig = plt.figure()
 obj = pd.read_csv(FILE_OBJ, delimiter = " ", names = ['R', 'Theta'])
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(9, 9))
+ax1 = fig.add_subplot()
 
 grid = sns.JointGrid(x = obj['R'], y = obj['Theta'], space = 0, height = 10, ratio = 8, xlim = [2, 15], ylim = [-400, 700])
 grid.plot_joint(plt.scatter, color = "black", s = 0.1)
@@ -22,8 +24,13 @@ sns.lineplot(x = lines['R'], y = lines['Theta'], palette = "tab10", linewidth = 
 sns.lineplot(x = lines['R'], y = lines['LTheta'], palette = "tab10", linewidth = 0.5, color = 'b')
 sns.lineplot(x = lines['R'], y = lines['HTheta'], palette = "tab10", linewidth = 0.5, color = 'b')
 
-sublines = lines[['R', 'Theta', 'LTheta', 'HTheta']][lines['R'] > 6.5]
-sublines = sublines[['R', 'Theta', 'LTheta', 'HTheta']][sublines['R'] < 10.6]
+low_R = 0
+high_R = 20
+low_th = -2000
+high_th = 2000
+
+sublines = lines[['R', 'Theta', 'LTheta', 'HTheta']][lines['R'] > low_R]
+sublines = sublines[['R', 'Theta', 'LTheta', 'HTheta']][sublines['R'] < high_R]
 sns.lineplot(x = sublines['R'], y = sublines['Theta'], palette = "tab12", linewidth = 3, color = 'r')
 max_p = sublines.loc[sublines['Theta'] == sublines['Theta'].max()]
 min_p = sublines.loc[sublines['Theta'] == sublines['Theta'].min()]
@@ -39,6 +46,20 @@ print("sqrt_sq = ", str(sqrt(sd ** 2 + sd2 ** 2)), ", fabs = ", abs(delta))
 FILE_SUN = sys.argv[3]
 sun = pd.read_csv(FILE_SUN, delimiter = " ", names = ['R', 'Theta'])
 sns.scatterplot(x = sun['R'], y = sun['Theta'], color = 'yellow', size = 4)
+#ax1.set_axis_label(['R, kpc', r'$\Theta$'])
+plt.savefig('theta_obj.png')
+
+plt.clf()
+grid = sns.JointGrid(x = obj['R'], y = obj['Theta'], space = 0, height = 10, ratio = 15, xlim = [low_R, high_R], ylim = [low_th, high_th])
+grid.plot_joint(plt.scatter, color = "black", s = 0.1)
+#_ = grid.ax_marg_y.hist(obj['Theta'], color = "black", orientation = "horizontal", bins = np.arange(low_th, high_th, 5))
+_ = grid.ax_marg_x.hist(obj['R'], color = "black", bins = np.arange(low_R, high_R, 0.1))
+FILE_LINE = sys.argv[2]
+lines = pd.read_csv(FILE_LINE, delimiter = " ", names = ['R', 'Theta', 'LTheta', 'HTheta'])
+sns.lineplot(x = lines['R'], y = lines['Theta'], palette = "tab10", linewidth = 2, color = 'b')
+sns.lineplot(x = lines['R'], y = lines['LTheta'], palette = "tab10", linewidth = 0.5, color = 'b')
+sns.lineplot(x = lines['R'], y = lines['HTheta'], palette = "tab10", linewidth = 0.5, color = 'b')
+sns.scatterplot(x = sun['R'], y = sun['Theta'], color = 'yellow', size = 10)
+plt.savefig("zoomed.png")
 
 
-plt.savefig("theta_obj.png")
--- sources/scripts/plottest.py
+++ sources/scripts/plottest.py
@@ -98,59 +98,91 @@ def view_residuals():
 
 def view_profile():
     plt.figure(figsize=(10, 10))
-    pt = pd.read_csv(str(sys.argv[2]), delimiter = " ", names = ['R_0', 'Sigma'])
-    g = sns.lineplot(x = "R_0", y="Sigma", data=pt, markers=True, linewidth=1.5, palette="tab10")
-    plt.savefig("profile.png")
+#    sns.JointGrid(x = obj['R'], y = obj['Theta'], space = 0, height = 10, ratio = 8, xlim = [2, 15], ylim = [-400, 700])
+    for i in range(1,int(sys.argv[3])):
+        pt = pd.read_csv(str(sys.argv[2]) + str(i) + str(sys.argv[4]), delimiter = " ", names = ['R_0', 'Sigma'])
+        sns.lineplot(x = "R_0", y="Sigma", data=pt, markers=True, linewidth=0.6, palette="tab10")
+    plt.savefig("profiles.png")
+
+def view_lb():
+    fig = plt.figure()
+    plt.figure(figsize=(10, 10))
+    ax1 = fig.add_subplot()
+    pt = pd.read_csv(str(sys.argv[2]), delimiter = " ", names = ['ra', 'dec', 'l', 'b', 'nv', 'vr', 'vr_err', 'r', 'pm_ra', 'pm_dec', 'pm_ra_err', 'pm_dec_err', 'match'])
+    ax1 = (sns.jointplot(x = 'l', y = 'b', data = pt, xlim = [0, 360], ratio = 5, ylim = [-90, 90], s=0.1, color="black")).plot_joint(plt.scatter, c="gray", s=0.1, linewidth=1, marker='+')
+    ax1.set_axis_labels("l, deg", "b, deg")
+    plt.savefig("lb.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['vr'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("vr_distr.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['vr_err'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("vr_err_distr.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['pm_ra'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("pm_ra_distr.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['pm_ra_err'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("pm_ra_err_distr.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['pm_dec'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("pm_dec_distr.png")
+    plt.clf()
+    ax1 = sns.distplot(pt['pm_dec_err'], kde = True, rug = False, bins = 100, color = 'black')
+    plt.savefig("pm_dec_err_distr.png")
+    plt.clf()
+
 
 def view_xyz():
     PATH=sys.argv[2]
-    NAME = sys.argv[3]
-    NAME_ERR = sys.argv[4]
+#    NAME = sys.argv[3]
+#    NAME_ERR = sys.argv[4]
     fig = plt.figure()
+    plt.figure(figsize=(10, 10))
     ax1 = fig.add_subplot()
 
     pt = pd.read_csv(PATH + '/final_xyz.txt', delimiter = " ", names = ['X', 'Y', 'Z'])
 
-    ax1 = (sns.jointplot(x = 'X', y = 'Y', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="b")).plot_joint(plt.scatter, c="b", s=0.1, linewidth=1, marker='+')
-    ax1.set_axis_labels("X", "Y")
+    ax1 = (sns.jointplot(x = 'X', y = 'Y', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="black")).plot_joint(plt.scatter, c="gray", s=0.1, linewidth=1, marker='+')
+    ax1.set_axis_labels("X, kpc", "Y, kpc")
     plt.savefig("XYobj.png")
 
-    ax1 = (sns.jointplot(x = 'X', y = 'Z', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="b")).plot_joint(plt.scatter, c="b", s=0.1, linewidth=1, marker='+')
-    ax1.set_axis_labels("X", "Z")
+    ax1 = (sns.jointplot(x = 'X', y = 'Z', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="black")).plot_joint(plt.scatter, c="gray", s=0.1, linewidth=1, marker='+')
+    ax1.set_axis_labels("X, kpc", "Z, kpc")
     plt.savefig("XZobj.png")
 
-    ax1 = (sns.jointplot(x = 'Y', y = 'Z', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="b")).plot_joint(plt.scatter, c="b", s=0.1, linewidth=1, marker='+')
-    ax1.set_axis_labels("Y", "Z")
-    plt.savefig("YZobj.png")
+#    ax1 = (sns.jointplot(x = 'Y', y = 'Z', data = pt, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="b")).plot_joint(plt.scatter, c="b", s=0.1, linewidth=1, marker='+')
+#    ax1.set_axis_labels("Y", "Z")
+#    plt.savefig("YZobj.png")
 
-    perrors = pd.read_csv(NAME, delimiter = " ", names = ['X', 'Y', 'Z'])
+#    perrors = pd.read_csv(NAME, delimiter = " ", names = ['X', 'Y', 'Z'])
 
-    ax2 = fig.add_subplot()
+#    ax2 = fig.add_subplot()
 
-    ax2 = (sns.jointplot(x = 'X', y = 'Y', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("X", "Y")
-    plt.savefig("XYmiss.png")
+#    ax2 = (sns.jointplot(x = 'X', y = 'Y', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("X", "Y")
+#    plt.savefig("XYmiss.png")
 
-    ax2 = (sns.jointplot(x = 'X', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("X", "Z")
-    plt.savefig("XZmiss.png")
+#    ax2 = (sns.jointplot(x = 'X', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("X", "Z")
+#    plt.savefig("XZmiss.png")
 
-    ax2 = (sns.jointplot(x = 'Y', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("Y", "Z")
-    plt.savefig("YZmiss.png")
+#    ax2 = (sns.jointplot(x = 'Y', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="green")).plot_joint(plt.scatter, c="green", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("Y", "Z")
+#    plt.savefig("YZmiss.png")
 
-    perrors = pd.read_csv(NAME_ERR, delimiter = " ", names = ['X', 'Y', 'Z'])
-    ax2 = (sns.jointplot(x = 'X', y = 'Y', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("X", "Y")
-    plt.savefig("XYerr.png")
+#    perrors = pd.read_csv(NAME_ERR, delimiter = " ", names = ['X', 'Y', 'Z'])
+#    ax2 = (sns.jointplot(x = 'X', y = 'Y', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("X", "Y")
+#    plt.savefig("XYerr.png")
 
-    ax2 = (sns.jointplot(x = 'X', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("X", "Z")
-    plt.savefig("XZerr.png")
+#    ax2 = (sns.jointplot(x = 'X', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("X", "Z")
+#    plt.savefig("XZerr.png")
 
-    ax2 = (sns.jointplot(x = 'Y', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
-    ax2.set_axis_labels("Y", "Z")
-    plt.savefig("YZerr.png")
+#    ax2 = (sns.jointplot(x = 'Y', y = 'Z', data = perrors, xlim = [-10, 10], ylim = [-10, 10], s=0.1, color="red")).plot_joint(plt.scatter, c="red", s=0.1, linewidth=1, marker='+')
+#    ax2.set_axis_labels("Y", "Z")
+#    plt.savefig("YZerr.png")
 
 
 
@@ -174,6 +206,8 @@ elif sys.argv[1] == "a":
     view_unf()
 elif sys.argv[1] == "r0":
     view_r0res()
+elif sys.argv[1] == "lb":
+    view_lb()
 else: 
     print("Nothing. Keys: s, v, r.")
 
